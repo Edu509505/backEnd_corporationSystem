32bd372076dfe9341375eff40561c5cfa9986902
Eduardo Nascimento <eduardonascimento60650@gmail.com>
2025-08-11 20:09:13 -0300

    resolvendo bug e adicionando uma nova tabela

--- controllers/controlePropostas.js
+++ controllers/controlePropostas.js
@@ -1,23 +1,23 @@
-import Proposta from "../models/propostas";
+import Proposta from "../models/propostas.js";
 
-async function createProposta(req, res){
+async function createProposta(req, res) {
     const { idCliente, tipoDeProposta } = req.body
     const propostas = await Proposta.create({ idCliente, tipoDeProposta })
 
-    if(propostas){
-        res.status(200).json({idCliente, tipoDeProposta})
-    }else {
-        res.status(500).json({message:'Não foi possivel criar'})
+    if (propostas) {
+        res.status(200).json({ idCliente, tipoDeProposta })
+    } else {
+        res.status(500).json({ message: 'Não foi possivel criar' })
     }
 }
 
-async function getProposta(req, res){
+async function getProposta(req, res) {
     const propostas = await Proposta.findAll()
 
-    if(propostas){
+    if (propostas) {
         res.json(propostas.map(propostas => propostas.toJSON()))
     } else {
-        res.status(500).json({message: 'Não foi possível buscar usuários'})
+        res.status(500).json({ message: 'Não foi possível buscar usuários' })
     }
 }
 
