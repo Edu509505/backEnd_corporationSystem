1a0d6e0eae89be2c18ecd6f9deffb5bbccab3fcb
nicholasacahu <nikdacap@gmail.com>
2025-09-04 19:49:04 -0300

    Update associations.js

--- models/associations.js
+++ models/associations.js
@@ -6,31 +6,19 @@ import Versionamento from './versionamento.js'
 import Anexo from './anexo.js'
 
 
-Clientes.hasMany(Proposta, {foreignKey: "idCliente", as: "proposta"})
+Clientes.hasMany(Contratos, {foreignKey: "idCliente", as: "contratos"})
+Contratos.belongsTo(Clientes, {foreignKey: "idCliente", as: "cliente"})
+//Clientes.hasMany(Proposta, {foreignKey: "idCliente", as: "proposta"})
 
 // UM CLIENTE ESTÁ ATRELADO A VÁRIAS PROPOSTA - 1:N
-Clientes.hasMany(Proposta, {foreignKey: "idCliente", as: "cliente"})
 Clientes.hasMany(Proposta, {foreignKey: "idCliente", as: "clienteProposta"})
 
 //UMA PROPOSTA TERÁ VÁRIOS VERSIONAMENTO - 1:N
 Proposta.hasMany(Versionamento, {foreignKey: "idProposta", as: "versionamento"})
 
-Versionamento.hasMany(Anexo, {foreignKey: ""})
-//Versionamento.hasMany(Anexo, {foreignKey: ""})Clientes.hasMany(Proposta, {foreignKey: "idCliente", as: "proposta"})
-
-// UM CLIENTE ESTÁ ATRELADO A VÁRIAS PROPOSTA - 1:N
-Clientes.hasMany(Proposta, {foreignKey: "idCliente", as: "cliente"})
-Clientes.hasMany(Proposta, {foreignKey: "idCliente", as: "clienteProposta"})
-
-//UMA PROPOSTA TERÁ VÁRIOS VERSIONAMENTO - 1:N
-Proposta.hasMany(Versionamento, {foreignKey: "idProposta", as: "versionamento"})
-
-Versionamento.hasMany(Anexo, {foreignKey: ""})
 //Versionamento.hasMany(Anexo, {foreignKey: ""})
 
-
 Medicoes.belongsTo(Contratos)
-
 
 
 /*
