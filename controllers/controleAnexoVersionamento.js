5b46f076f99ec1520fb19702e0a1c497b7581cab
Eduardo Nascimento <eduardonascimento60650@gmail.com>
2025-09-08 01:32:49 -0300

    ***

--- controllers/controleAnexoVersionamento.js
+++ controllers/controleAnexoVersionamento.js
@@ -16,29 +16,6 @@ async function createAnexoVersionamento(req, res) {
         // Esepra as informações do req.body cria um novo registro
         const imagens = await Anexo.create({ idVersionamento, path })
 
-        //UPANDO ARQUIVO
-        const filePath = path.join(import.meta.dirname, '..', 'temp', req.file.filename);
-        //Explicando a linha: o path.join() é uma função que junta diferentes partes de um caminho,
-        // o import.meta.dirname está dizedo para o código "Estou nessa pasta"
-        // logo após os '..', 'temp' é como se você estivesse fazendo '../temp/arquivo
-        // req.file.filename é o nome do aqrquivo que eu estou colocando em meu diretório
-
-
-        console.log('filePath', filePath)
-
-        const file = readFileSync(filePath);
-        //essa variável está lendo o arquivo
-
-        const extensaoDoArquivo = req.file.originalname.split('.').reverse()[0];
-
-        const command = new PutObjectCommand({
-            Bucket: 'anexo-versionamento',
-            Key: `/${imagens.idVersionamento}/${imagens.id}.${extensaoDoArquivo}`,
-            Body: file
-        });
-
-        await s3.send(command);
-
         if (imagens) {
             res.status(200).json({ idVersionamento, path })
         }
--- controllers/controlePropostas.js
+++ controllers/controlePropostas.js
@@ -17,22 +17,30 @@ async function createProposta(req, res) {
 
     const versionamento = await Versionamento.create({ idProposta: proposta.id, versao: 1, status: 'EM_ANALISE' });
 
-    await AnexoVersionamento.create({ idVersionamento: versionamento.id, path: "" })
+    //UPANDO ARQUIVO
+    const filePath = path.join(import.meta.dirname, '..', 'temp', req.file.filename);
+    //Explicando a linha: o path.join() é uma função que junta diferentes partes de um caminho,
+    // o import.meta.dirname está dizedo para o código "Estou nessa pasta"
+    // logo após os '..', 'temp' é como se você estivesse fazendo '../temp/arquivo
+    // req.file.filename é o nome do aqrquivo que eu estou colocando em meu diretório
 
-    // const filePath = path.join(import.meta.dirname, '..', 'temp', req.file.filename);
-    // console.log('filePath', filePath)
 
-    // const file = readFileSync(filePath);
+    console.log('filePath', filePath)
 
-    // const command = new PutObjectCommand({
-    //     Bucket: 'anexo-versionamento',
-    //     Key: `/${versionamento.idProposta}/${versionamento.id}.${extensao}`,
-    //     Body: file
-    // });
+    const file = readFileSync(filePath);
+    //essa variável está lendo o arquivo
 
-    // console.log('caminho do arquivo com extensao: ', command)
+    const extensaoDoArquivo = req.file.originalname.split('.').reverse()[0];
 
-    // await s3.send(command);
+    const command = new PutObjectCommand({
+        Bucket: 'anexo-versionamento',
+        Key: `/${versionamento.idProposta}/${versionamento.id}.${extensaoDoArquivo}`,
+        Body: file
+    });
+
+    await s3.send(command);
+
+    await AnexoVersionamento.create({ idVersionamento: versionamento.id, path: filePath })
 
     if (proposta) {
         res.status(200).json({ idCliente, nomeDaProposta, descricao, id: proposta.id })
