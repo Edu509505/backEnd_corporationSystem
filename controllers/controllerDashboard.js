9cebaec433b2429fb5ee1d9545e1ec5fbf40096a
nicholasacahu <nikdacap@gmail.com>
2025-11-11 20:40:58 -0300

    Update controllerDashboard.js

--- controllers/controllerDashboard.js
+++ controllers/controllerDashboard.js
@@ -26,7 +26,7 @@ async function dashboard(req, res) {
       FROM 
         diarioDeObra d
       JOIN 
-        ItensDoDia i ON i.idDiarioDeObra = d.id
+        itensDoDia i ON i.idDiarioDeObra = d.id
       WHERE 
         d.dataDia BETWEEN :dataInicial AND :dataFinal
       GROUP BY 
